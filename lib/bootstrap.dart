0de59eb0481bcca625b5b4a3cded9ba245510688
Ibrahim Aboelyazed <ebrahem6076@gmail.com>
2024-05-05 14:01:03 +0300

    Added bootstrap for running app and adding observers for bloc

---
+++ lib/bootstrap.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipes_app/utils/utils.dart';
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  Bloc.observer = const AppBlocObserver();
+
+  await runZonedGuarded(
+    () async {
+      await AppHelper.configureApp();
+      runApp(await builder());
+    },
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
+}
